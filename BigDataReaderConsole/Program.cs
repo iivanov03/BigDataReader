c102fa7866549c11f322ad3b4108a26ef4dbc050
Ivan <vanio5354@gmail.com>
2023-12-17 11:47:48 +0200

    Add csv reader

--- BigDataReaderConsole/Program.cs
+++ BigDataReaderConsole/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+using CsvHelper;
+using CsvHelper.Configuration;
+
+using BigDataReaderConsole;
+
+string filePath = "data" + "/" + "data.csv";
+
+try
+{
+    using (var reader = new StreamReader(filePath))
+    using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+    {
+        var records = csv.GetRecords<OrganizationModel>().ToList();
+        foreach (var record in records)
+        {
+            Console.WriteLine($"Name: {record.Name}, Country: {record.Country}, Industry: {record.Industry}");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
